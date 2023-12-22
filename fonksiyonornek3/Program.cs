f662d7aeb59566f9e0050307f54d249a9038eb24
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2023-12-22 10:44:21 +0300

    Proje dosyası ekle.

---
+++ fonksiyonornek3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fonksiyonornek3
+{
+    internal class Program
+    {
+        static double donustur(double f)
+        {
+            double c;
+            c =(Convert.ToDouble(5) / Convert.ToDouble(9))*(f - 32);
+            //c = ((Double)(5) / (Double)(9)) * (f - 32); //bir üst satır ile aynı
+            return c;
+        }
+        static void Main(string[] args)
+        {
+            /*
+             Örnek: Kendisine gönderilen fahrenhayt (F) değerini 
+            derece (C) ye çevirip geri döndüren fonksiyonu yazınız
+             */
+            double fh,s;
+            Console.WriteLine("Fahrenhayt gir:");
+            fh=double.Parse(Console.ReadLine());
+            s=donustur(fh);
+            Console.WriteLine("Sonuc={0}", s);
+        }
+    }
+}
